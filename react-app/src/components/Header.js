aa6f34257c38759458cf2cc1511c2f0f9f0a5552
Оксана Анощенко <anoschenko.ov@phystech.edu>
2021-07-07 12:01:06 +0300

    replaced Link to NavLink

--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from '../styles/Header.module.css';
 
 function Header({ activePath }) {
   return (
     <div className={styles.HeaderContainer}>
-      <Button currentPath='/users' text='Users' activePath={activePath} />
-      <Button currentPath='/products' text='Products' activePath={activePath} />
+      <Tab currentPath='/users' text='Users' activePath={activePath} />
+      <Tab currentPath='/products' text='Products' activePath={activePath} />
     </div>
   );
 }
 
-function Button({ currentPath, text, activePath }) {
-  let style = `${styles.Link}`;
-  
-  if (currentPath === activePath) {
-    style = `${styles.Link} ${styles.ActiveButton}`;
-  }
-
+function Tab({ currentPath, text }) {
   return (
-    <Link className={style} to={currentPath}>{text}</Link>
+    <NavLink className={styles.Link} activeClassName={styles.ActiveButton} to={currentPath}>
+      {text}
+    </NavLink>
   );
 }
 
